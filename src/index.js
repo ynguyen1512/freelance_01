2cb61295d37a07aaf5a0f8824f70f89d86ebc03d
Richard Nguyen <nguyenphuyfgw@gmail.com>
2024-11-27 00:43:47 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './output.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // src/index.js or src/App.js
+import "@fontsource/plus-jakarta-sans"; // Import the font here
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
